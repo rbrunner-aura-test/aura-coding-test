1048cb8e4bdc6446724a86e00d27bff18373f3cf
Rowyn Brunner <rb5sq@virginia.edu>
2021-02-25 20:08:25 -0500

    answers

--- src/pages/GlobalRecords/index.js
+++ src/pages/GlobalRecords/index.js
@@ -1,25 +1,70 @@
 // Globals
-import React from "react";
+import React, { createContext, useState, useEffect, useContext } from "react";
 
 // Components
 import { Record } from "components/Record";
 
 // Misc
 import { data } from "components/Record/data";
+import { mockFetch } from "util/mockFetch";
+
+const RecordContext = createContext({
+  records: []
+});
+
+function useRecords() {
+  const contextValue = useContext(RecordContext);
+  if (!contextValue) {
+    throw new Error("can't use record context in this scope");
+  } else {
+    return contextValue;
+  }
+}
 
 // Component
 function GlobalRecords() {
+  const [records, setRecords] = useState([]);
+  const [couldntRetrieve, setCouldntRetrieve] = useState(false);
+  useEffect(() => {
+    (async () => {
+      mockFetch()()
+      .then(retrievedRecords => {
+        console.log("here");
+        setRecords(retrievedRecords);
+      })
+      .catch(error =>{
+        setCouldntRetrieve(true);
+        console.error(error);
+      });
+    })();
+  }, []);
   return (
-    <div className="aura-page aura-global_records">
-      <h1>Top Records of 2020</h1>
+    <RecordContext.Provider value={{ records }}>
+      <div className="aura-page aura-global_records">
+        {couldntRetrieve ? <ErrorMessage/> : <RecordList/>}
+      </div>
+    </RecordContext.Provider>
+  );
+}
 
+function ErrorMessage() {
+  return (
+    <h1>Sorry, a problem occurred</h1>
+  );
+}
+
+function RecordList() {
+  const records = useRecords();
+  return (
+    <>
+      <h1>Top Records of 2020</h1>
       <div className="aura-page-content">
-        {data.map((record) => {
+        {records.records.map((record) => {
           return <Record key={record.id} data={record} />;
         })}
       </div>
-    </div>
-  );
+    </>
+  )
 }
 
 export { GlobalRecords };
--- src/pages/Timer/index.js
+++ src/pages/Timer/index.js
@@ -1,6 +1,6 @@
 // Globals
 import "./styles.scss";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // Components
 import { Button } from "components/Button";
@@ -15,25 +15,47 @@ function Expired() {
   );
 }
 
+function ActiveTimer(props) {
+  return (
+    <div>{formatTime(props.seconds)}</div>
+  );
+}
+
+function formatTime(seconds) {
+  const displayMinutes = Math.floor(seconds / 60);
+  const displaySeconds = seconds % 60;
+  const secondString = displaySeconds >= 10 ? `${displaySeconds}` : `0${displaySeconds}`;
+  return `${displayMinutes}:${secondString}`;
+}
+
 // Component
 function Timer() {
   // Hooks - state
   const [counter, setCounter] = useState(0);
-
+  const [running, setRunning] = useState(false);
   // TODO: implement counter...
+  useEffect(() => {
+    if (running) {
+      setTimeout(() => {
+        if (counter > 0) {
+          setCounter(counter - 1);
+        }
+      }, 1000);
+    }
+  }, [running, counter]);
 
   // Render
   return (
     <div className="aura-page aura-timer">
       <h1>Timer</h1>
-
       <div className="aura-page-content">
-        <div className="aura-timer-clock">0:00</div>
+        <div className="aura-timer-clock">
+          <ActiveTimer seconds={counter}/>
+        </div>
         {counter <= 0 ? <Expired /> : null}
-
         <div className="aura-timer-buttons">
-          <Button>Start</Button>
-          <Button>Reset</Button>
+          <Button onClick={() => setRunning(true)}>Start</Button>
+          <Button onClick={() => setCounter(60)}>Reset</Button>
         </div>
       </div>
     </div>
